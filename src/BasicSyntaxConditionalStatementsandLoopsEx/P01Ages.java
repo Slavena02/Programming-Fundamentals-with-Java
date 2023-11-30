14d065b3dac9d070c28d2cb902ec4909be1247f6
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-12-01 00:34:19 +0200

    add new exercise

--- src/BasicSyntaxConditionalStatementsandLoopsEx/P01Ages.java
+++ src/BasicSyntaxConditionalStatementsandLoopsEx/P01Ages.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class P01Ages {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
+        int age = Integer.parseInt(scanner.nextLine());
+
     }
 }
