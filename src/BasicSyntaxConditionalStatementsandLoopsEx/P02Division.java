d1cf62ddf7aadc596645818c40e0bbe157756371
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-12-01 00:46:39 +0200

    add new exercise

--- src/BasicSyntaxConditionalStatementsandLoopsEx/P02Division.java
+++ src/BasicSyntaxConditionalStatementsandLoopsEx/P02Division.java
@@ -6,5 +6,8 @@ public class P02Division {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int number = Integer.parseInt(scanner.nextLine());
+
+
     }
 }
