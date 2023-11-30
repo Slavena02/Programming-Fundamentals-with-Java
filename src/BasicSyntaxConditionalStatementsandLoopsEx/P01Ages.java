20c4e142a37693be2d0a207dbae10cbe78250bd3
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-12-01 00:32:22 +0200

    Initial commit

---
+++ src/BasicSyntaxConditionalStatementsandLoopsEx/P01Ages.java
@@ -0,0 +1,10 @@
+package BasicSyntaxConditionalStatementsandLoopsEx;
+
+import java.util.Scanner;
+
+public class P01Ages {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
