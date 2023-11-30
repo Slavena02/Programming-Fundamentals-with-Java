2ecea69efc8953b70dfde1691b7136e41d1c9fbd
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-12-01 00:44:33 +0200

    add new exercise

---
+++ src/BasicSyntaxConditionalStatementsandLoopsEx/P02Division.java
@@ -0,0 +1,10 @@
+package BasicSyntaxConditionalStatementsandLoopsEx;
+
+import java.util.Scanner;
+
+public class P02Division {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
