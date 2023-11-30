d8fbea94f0528333dff889c5d1f3d0f87299977d
Slavena02 <132493721+Slavena02@users.noreply.github.com>
2023-12-01 00:41:53 +0200

    add new exercise

--- src/BasicSyntaxConditionalStatementsandLoopsEx/P01Ages.java
+++ src/BasicSyntaxConditionalStatementsandLoopsEx/P01Ages.java
@@ -8,5 +8,16 @@ public class P01Ages {
 
         int age = Integer.parseInt(scanner.nextLine());
 
+        if (age <= 2) {
+            System.out.println("baby");
+        } else if (age <= 13) {
+            System.out.println("child");
+        } else if (age <= 19) {
+            System.out.println("teenager");
+        } else if (age <= 65) {
+            System.out.println("adult");
+        }else{
+            System.out.println("elder");
+        }
     }
 }
